9d832713535cba25c193d603deca4a16c07c019c
Renz Frederick <d3nzus@gmail.com>
2026-05-18 12:45:36 +0800

    refactor: atomized DBConnector into seperate php files for encapsulation

---
+++ src/backend/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// centralized credentials
+$host = "localhost";
+$dbname = "upcart";
+$user = "root";
+$password = "";
+
+?>
---
+++ src/backend/config/connector.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__ . "/config.php"
+
+    try{
+        # create PDO instance
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+
+        # set error mode to exception for debugging
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (PDOException $e) {
+        # handle connection error
+        die("Database connection failed: " . $e->getMessage());
+?>
