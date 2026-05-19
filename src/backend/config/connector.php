b1b6ba19b498e4f4ce62f3d53dbfc6e0e9bc7c46
rykonnen <lqblancaflor@up.edu.ph>
2026-05-20 00:36:19 +0800

    fix: missing semi-colon

--- src/backend/config/connector.php
+++ src/backend/config/connector.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . "/config.php"
+    require_once __DIR__ . "/config.php";
 
     try{
         # create PDO instance
@@ -11,4 +11,5 @@
     } catch (PDOException $e) {
         # handle connection error
         die("Database connection failed: " . $e->getMessage());
+    }
 ?>
